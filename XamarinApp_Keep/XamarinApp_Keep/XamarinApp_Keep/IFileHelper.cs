ca221e3e178498ffcfd6f9430063680e5119d15b
Lukáš Breburda <breburda.dolakova@gmail.com>
2017-05-23 08:59:26 +0200

    kk

---
+++ XamarinApp_Keep/XamarinApp_Keep/XamarinApp_Keep/IFileHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinApp_Keep
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string filename);
+    }
+}
